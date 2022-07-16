cf0be6e9a627e632fc13259a6206b9cba4c0ea0f
Gabriel Rodrigues <gabrielrf.dev2050@gmail.com>
2022-07-16 09:17:56 -0300

    desafio Bar Final

--- bar.c
+++ bar.c
@@ -6,7 +6,7 @@ main()
 {
     char sexo; 
     int ingressoH = 10; 
-   /* int ingressoM = 8;  */
+    int ingressoM = 8;  
     int cerveja = 5; 
     int refrigerante =  3; 
     int espetinho = 7; 
@@ -29,6 +29,7 @@ main()
         scanf ("%d", &quantidaderefrigerante); 
              printf("\n");
         consumo = (quantidadecerveja*cerveja) + (quantidadeespetinho*espetinho) + (quantidaderefrigerante*refrigerante);
+        
         if (consumo > 30)
         {
             total = consumo + ingressoH;  
@@ -48,7 +49,8 @@ main()
         }
         else
          {
-            total = consumo + couvert + ingressoH; 
+            
+             total = consumo + couvert + ingressoH; 
             
             printf ("Quantidade de Cervejas   %d",  quantidadecerveja); 
                 printf("\n");
@@ -63,6 +65,58 @@ main()
             printf ("O total da conta é  %d",total); 
          }
     }
+
+    if (sexo == 'F'|| sexo == 'f')
+    {
+        printf ("Digite a quantidade de cerveja\t");
+        scanf ("%d", &quantidadecerveja); 
+
+        printf ("Digite a quantidade de Espetinho\t");
+        scanf ("%d", &quantidadeespetinho); 
+
+        printf ("Digite a quantidade de Refrigerante\t");
+        scanf ("%d", &quantidaderefrigerante); 
+             printf("\n");
+        consumo = (quantidadecerveja*cerveja) + (quantidadeespetinho*espetinho) + (quantidaderefrigerante*refrigerante);
+
+        if (consumo > 30)
+        {
+             total = consumo + ingressoH;  
+
+            printf ("Quantidade de Cervejas    %d\n",  quantidadecerveja); 
+                printf("\n");
+            printf ("Quantidade de Espetinhos  %d",  quantidadeespetinho); 
+                printf("\n");
+            printf ("Quantidade de Cervejas    %d",  quantidaderefrigerante); 
+                printf("\n");
+            printf ("counvert R$ 0,00");
+                printf("\n");
+            printf ("Ingresso R$ %d",ingressoM);
+                printf("\n");
+            printf ("O total da conta é  R$ %d",total);
+        }
+        else 
+        {
+             total = consumo + couvert + ingressoM; 
+            
+            printf ("Quantidade de Cervejas   %d",  quantidadecerveja); 
+                printf("\n");
+            printf ("Quantidade de Espetinhos %d",  quantidadeespetinho); 
+                 printf("\n");
+            printf ("Quantidade de Cervejas   %d",  quantidaderefrigerante); 
+                printf("\n");
+            printf ("counvert R$ ",ingressoM );
+                printf("\n");
+            printf ("Ingresso R$ ",ingressoM);
+                printf("\n");
+            printf ("O total da conta é  %d",total); 
+        }
+       
+    }
+    if (sexo != 'M' || sexo !='m'|| sexo != 'F'|| sexo != 'f')
+        {
+           printf ("valor inválido"); 
+        }
 
 
     return 0; 
