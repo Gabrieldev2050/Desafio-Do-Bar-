3ad3cf5fea0eaf254fc5639cc21150d4bba7ebf8
Gabriel Rodrigues <gabrielrf.dev2050@gmail.com>
2022-07-16 08:36:22 -0300

    definição de variaveis

---
+++ bar.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h> 
+
+main() 
+{
+    char sexo; 
+    int ingressoH = 10; 
+    int ingressoM = 8;  
+    int cerveja = 5; 
+    int refrigerante =  3; 
+    int espetinho = 7; 
+    int couvert = 4; 
+
+    printf ("Teste!\n"); 
+
+
+    return 0; 
+}
